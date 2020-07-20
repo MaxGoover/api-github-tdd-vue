c77525ffa6b5a4de135a9a1a44172eec28f88e9a
maxgoover <maxgoover@gmail.com>
2020-07-21 01:13:12 +0300

    Added first view and made snapshot

---
+++ src/views/UserView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello UserView
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserView'
+}
+</script>
+
+<style scoped>
+
+</style>
--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,3 +1,16 @@
+import {shallowMount} from '@vue/test-utils'
+import UserView from '@/views/UserView'
+
 describe('UserView', () => {
-    it('works', () => {})
+  it('renders the component', () => {
+    // shallowMount - создаст экземпляр нашего компонента
+    const wrapper = shallowMount(UserView)
+
+    // assert
+    expect(wrapper.html()).toMatchSnapshot()
+    /*wrapper, который является представлением нашего компонента,
+    созданного vue-test-utils, а затем мы «фотографируем» html
+    нашего компонента (создаем snapshot). Этот HTML существует
+    благодаря vue-test-utils.*/
+  })
 })
