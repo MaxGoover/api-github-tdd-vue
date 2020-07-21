3ce0e71493703e00bfd571f0237150620d3a3789
maxgoover <maxgoover@gmail.com>
2020-07-21 21:27:56 +0300

    Added test api

--- src/api.js
+++ src/api.js
@@ -1,1 +1,15 @@
-export default {}
+import axios from 'axios'
+import httpAdapter from 'axios/lib/adapters/http'
+
+const instance = axios.create({
+  adapter: httpAdapter,
+  baseURL: 'https://api.github.com',
+})
+
+export default {
+  searchUser(username) {
+    return instance
+      .get(`/users/${ username }`)
+      .then(result => result.data)
+  }
+}
