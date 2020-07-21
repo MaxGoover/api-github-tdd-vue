453053ef77957ecbb2cdc5247b4312519cefaf69
maxgoover <maxgoover@gmail.com>
2020-07-21 22:06:22 +0300

    Refactored test for UserDearchForm

--- src/components/VUserSearchForm.vue
+++ src/components/VUserSearchForm.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <h1>VUserSearchForm</h1>
-    <form @submit.prevent="$emit('submitted', username)">
-      <input v-model="username" type="text"/>
-      <button type="submit">Search</button>
-    </form>
-  </div>
+  <form @submit.prevent="$emit('submitted', username)">
+    <el-input
+      v-model="username"
+      class="search-form__input"
+      placeholder="Enter username"
+    >
+      <el-button
+        class="search-form__button"
+        icon="el-icon-search"
+        native-type="submit"
+        slot="append"
+      />
+    </el-input>
+  </form>
 </template>
 
 <script>
