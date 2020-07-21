b0080c852268ce30de5b7f61ee637edb2d72822b
maxgoover <maxgoover@gmail.com>
2020-07-21 21:45:02 +0300

    Installed ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import './registerServiceWorker'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import Element from 'element-ui'
 import router from './router'
 import store from './store/index'
 import Vue from 'vue'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
